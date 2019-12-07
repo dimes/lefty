cdcdddae85bcb542d01e0ccf93124309a4d96c73
dimes <mgale4@binghamton.edu>
2019-12-07 13:01:03 -0500

    Fix build

--- pipeline-serialization/src/main/kotlin/lefty/pipeline/serialization/SerializedSpecification.kt
+++ pipeline-serialization/src/main/kotlin/lefty/pipeline/serialization/SerializedSpecification.kt
@@ -5,9 +5,10 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import lefty.pipeline.Specification
 
 class SerializedSpecification @JsonCreator constructor(
+        @JsonProperty("name") val name: String,
         @JsonProperty("steps") val steps: List<SerializedStep>
 ) {
     fun toSpecification(): Specification {
-        return Specification(steps.map(SerializedStep::toPipelineStep))
+        return Specification(name, steps.map(SerializedStep::toPipelineStep))
     }
 }
